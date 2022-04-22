08f0d864cbfd620db911b30f1b32502d89d70b1e
Naresh Kukkala <nareshkukkala28@gmail.com>
2022-04-22 16:37:52 +0530

    Async Image in Swift UI

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by naresh kukkala on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
